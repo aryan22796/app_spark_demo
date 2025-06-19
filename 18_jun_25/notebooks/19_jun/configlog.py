437692771ed80c0401a956eb16c2878bfeedfaa2
nagendra <aryan22796gmail.com>
2025-06-19 09:49:47 +0530

    update

--- 18_jun_25/notebooks/19_jun/configlog.py
+++ 18_jun_25/notebooks/19_jun/configlog.py
@@ -71,10 +71,14 @@ from pyspark.sql.functions import col
 spark = (
     SparkSession.builder
     .appName("log propr")
+    .config("spark.executor.cores", "2")
+    .config("spark.executor.memory", "2g")
+    .config("spark.executor.memoryOverhead", "1g")
+    .config("spark.sql.shuffle.partitions","10")
     .getOrCreate()
 )
 print("all partition ", spark.conf.get("spark.sql.shuffle.partitions"))
-
+spark.sparkContext.setLogLevel("Warn")#pep8, pyspark, python,github(git config --global user.name "",user.email)
 data = [(1, "Alice",20000), (2, "Bob",30000), (3, "Cathy",45000), (4, "David",50000)]
 df = spark.createDataFrame(data, ["id", "name","Salary"])
 b_df = df.withColumn("bonus",col("Salary")*.10)
