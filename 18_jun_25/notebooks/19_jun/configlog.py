2d4b842df3b4711f32c4d89d95d2e56898bf7ec3
nagendra <aryan22796gmail.com>
2025-06-19 09:57:37 +0530

    output

--- 18_jun_25/notebooks/19_jun/configlog.py
+++ 18_jun_25/notebooks/19_jun/configlog.py
@@ -79,7 +79,12 @@ spark = (
 )
 print("all partition ", spark.conf.get("spark.sql.shuffle.partitions"))
 spark.sparkContext.setLogLevel("Warn")#pep8, pyspark, python,github(git config --global user.name "",user.email)
+
 data = [(1, "Alice",20000), (2, "Bob",30000), (3, "Cathy",45000), (4, "David",50000)]
 df = spark.createDataFrame(data, ["id", "name","Salary"])
+#df = spark.read.parquet("file/PAth")
+
 b_df = df.withColumn("bonus",col("Salary")*.10)
+b_df = df.filter("Salary >2500")
 b_df.show()
+
